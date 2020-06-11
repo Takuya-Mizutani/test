2e2664ee7f8b9a8cb2206d1f913ada7b8b5ec203
Takuya Mizutani <Takuya-Mizutani@users.noreply.github.com>
2020-06-11 23:12:26 +0900

    ok

--- mysite/manage.py
+++ mysite/manage.py
@@ -3,8 +3,9 @@
 import os
 import sys
 
-sys.path.append("/private/var/mobile/Containers/Shared/AppGroup/BF6D28F7-8CDD-43FE-AAB3-F1369C817B0C/File Provider Storage/Repositories/test_project/mysite")
-
+path_ = sys.path[0]
+# sys.path.append("/private/var/mobile/Containers/Shared/AppGroup/BF6D28F7-8CDD-43FE-AAB3-F1369C817B0C/File Provider Storage/Repositories/test_project/mysite")
+sys.path.append(path_)
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
